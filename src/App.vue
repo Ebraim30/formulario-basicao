c089d7261aa025293239dece467a151219570c32
JoaoDums <joaopedro23342345555@gmail.com>
2024-03-18 20:26:07 -0300

    adicionado a trava que não deixa cadastrar se todos os campos não estiverem preenchidos.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,43 @@
 <script setup>
-import { normalizeStyle, ref } from 'vue';
-import { reactive } from 'vue';
+import { normalizeStyle, ref } from 'vue'
+import { reactive } from 'vue'
+import Swal from 'sweetalert2'
 
 const registrar = ref(false)
 
 const user = reactive({
-  avatar: null,
-});
+  avatar: null
+})
 
- 
-const nome = ref ('')
-const email = ref ('')
-const senha = ref ('')
-const confirmacao = ref ('')
-const profissao = ref ('')
-const salario = ref ('')
-const cpf = ref  ('')
-const linguagens = ref ('')
-const idade = ref ('') 
-const Endereco = ref ('')
-const cidade = ref ('')
-const data = ref ('')
-const biografia = ref ('')
+const nome = ref('')
+const email = ref('')
+const senha = ref('')
+const confirmacao = ref('')
+const profissao = ref('')
+const salario = ref('')
+const cpf = ref('')
+const linguagens = ref('')
+const idade = ref('')
+const Endereco = ref('')
+const cidade = ref('')
+const data = ref('')
+const biografia = ref('')
 
 function deletar(apagado) {
-  nome.value = ('')
-  email.value = ('')
-  senha.value = ('')
-  confirmacao.value = ('')
-  profissao.value = ('')
-  salario.value = ('')
-  cpf.value = ('')
-  linguagens.value = ('')
-  idade.value = ('')
-  Endereco.value = ('')
-  cidade.value = ('')
-  data.value = ('')
-  biografia.value = ('')
-  registrar.value = (false)
+  nome.value = ''
+  email.value = ''
+  senha.value = ''
+  confirmacao.value = ''
+  profissao.value = ''
+  salario.value = ''
+  cpf.value = ''
+  linguagens.value = ''
+  idade.value = ''
+  Endereco.value = ''
+  cidade.value = ''
+  data.value = ''
+  biografia.value = ''
+  registrar.value = false
 }
 
 function edit(editar) {
@@ -52,49 +52,187 @@ function edit(editar) {
   idade.value = idade.value
   Endereco.value = Endereco.value
   cidade.value = cidade.value
-  data.value = data.value  
+  data.value = data.value
   biografia.value = biografia.value
-  registrar.value = (false)
+  registrar.value = false
+}
+
+function save(value) {
+if (nome.value == '' || nome.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (email.value == '' || email.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+
+  if (senha.value == '' || senha.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (confirmacao.value == '' || confirmacao.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (profissao.value == '' || profissao.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (salario.value == '' || salario.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (cpf.value == '' || cpf.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (linguagens.value == '' || linguagens.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (idade.value == '' || idade.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campozão!!!",
+  icon: "error"
+});
+    return
+  }
+  if (Endereco.value == '' || Endereco.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campo!",
+  icon: "error"
+});
+    return
+  }
+  if (cidade.value == '' || cidade.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campozão!!!",
+  icon: "error"
+});
+    return
+  }
+  if (data.value == '' || data.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campozão!!!",
+  icon: "error"
+});
+    return
+  }
+
+  if (biografia.value == '' || biografia.value == null) {
+    Swal.fire({
+  title: "Atenção",
+  text: "você esqueceu de algum campozão!!!",
+  icon: "error"
+});
+    return
+  }
 }
 
 
-
 function handleFileUpload(e) {
-  const target = e.target;
+  const target = e.target
   if (target && target.files) {
-    const file = target.files[0];
-    user.avatar = URL.createObjectURL(file);
+    const file = target.files[0]
+    user.avatar = URL.createObjectURL(file)
   }
 }
 </script>
 
 <template>
   <div class="formulario">
-    <h1> Informe essas informacoes </h1>
-    
+    <h1>Informe essas informacoes</h1>
+
     <input type="file" id="avatarField" @change="handleFileUpload($event)" />
     <img v-if="user.avatar" :src="user.avatar" />
-    
-    <p>Informe seu nome: </p>
-    <input type="text" v-model="nome" size="20" maxlength="20" placeholder="Digite seu nome">
+
+    <p>Informe seu nome:</p>
+    <input type="text" v-model="nome" size="20" maxlength="20" placeholder="Digite seu nome" />
     <p>Informe seu email:</p>
-    <input type="email" v-model="email" size="20" maxlength="30" placeholder="Digite seu email">
+    <input type="email" v-model="email" size="20" maxlength="30" placeholder="Digite seu email" />
     <p>Informe sua senha:</p>
-    <input type="password" v-model="senha" size="20" maxlength="20" placeholder="Digite sua senha:">
+    <input
+      type="password"
+      v-model="senha"
+      size="20"
+      maxlength="20"
+      placeholder="Digite sua senha:"
+    />
     <p>Confirme sua Senha</p>
-    <input type="password" v-model="confirmacao" size="20" maxlength="20" placeholder="Confirme sua senha">
+    <input
+      type="password"
+      v-model="confirmacao"
+      size="20"
+      maxlength="20"
+      placeholder="Confirme sua senha"
+    />
     <p>Informe sua profissao</p>
-    <input type="text" v-model="profissao" size="20" maxlength="30" placeholder="Digite sua Profissão">
+    <input
+      type="text"
+      v-model="profissao"
+      size="20"
+      maxlength="30"
+      placeholder="Digite sua Profissão"
+    />
     <p>Informe seu salario</p>
-    <input type="password" v-model="salario" size="20" maxlength="30" placeholder="Digite seu salario">
-    <p>Informe seu cpf: </p>
-    <input type="password" v-model="cpf" size="20" maxlength="11" placeholder="Digite seu CPF">
-    <p>Informe sua idade: </p>
-    <input type="number" v-model="idade" size="20" placeholder="Digite sua idade">
+    <input
+      type="password"
+      v-model="salario"
+      size="20"
+      maxlength="30"
+      placeholder="Digite seu salario"
+    />
+    <p>Informe seu cpf:</p>
+    <input type="password" v-model="cpf" size="20" maxlength="11" placeholder="Digite seu CPF" />
+    <p>Informe sua idade:</p>
+    <input type="number" v-model="idade" size="20" placeholder="Digite sua idade" />
     <p>Informe a Data</p>
-    <input type="date" v-model="data" placeholder="Digite a Data">
+    <input type="date" v-model="data" placeholder="Digite a Data" />
     <p>Informe seu Endereco</p>
-    <input type="string" v-model="Endereco" size="20" maxlength="55" placeholder="Digite sua Rua e Nº">
+    <input
+      type="string"
+      v-model="Endereco"
+      size="20"
+      maxlength="55"
+      placeholder="Digite sua Rua e Nº"
+    />
     <p>Informe seu Estado</p>
     <select v-model="cidade">
       <option value="AC">Acre</option>
@@ -125,89 +263,60 @@ function handleFileUpload(e) {
       <option value="TO">Tocantins</option>
       <option value="DF">Distrito Federal</option>
     </select>
-    <hr>
+    <hr />
     <p>Escolha uma Linguagem</p>
     <select v-model="linguagens">
-    <option value="JS">Javascript</option>
-    <option value="VUE">VUEJS</option>
-    <option value="HTML">HTML4</option>
-    <option value="C++">C++</option>
-    <option value="C#">C#</option>
-    <option value="CSS">CSS</option>
-    <option value="PHP">PHP</option>
-    <option value="Python">Python</option>
-    <option value="Django">Django</option>
+      <option value="JS">Javascript</option>
+      <option value="VUE">VUEJS</option>
+      <option value="HTML">HTML4</option>
+      <option value="C++">C++</option>
+      <option value="C#">C#</option>
+      <option value="CSS">CSS</option>
+      <option value="PHP">PHP</option>
+      <option value="Python">Python</option>
+      <option value="Django">Django</option>
     </select>
-    <hr>
+    <hr />
     <p>Biografia</p>
     <textarea v-model="biografia" cols="30" rows="10"></textarea>
 
-
-  <div class="user-cards">
-    <button  @click="$event => registrar = !registrar">Registrar</button> 
-    <div class="card">Dados Salvos
-    <div v-if="registrar" class="card-content">
-      <div>
-       Foto: <img :src="user.avatar" />
+    <div class="user-cards">
+      <button @click="save()">Registrar</button>
+      <div class="card">
+        Dados Salvos
+        <div v-if="registrar" class="card-content">
+          <div>Foto: <img :src="user.avatar" /></div>
+          <div>Nome: {{ nome.value }}</div>
+          <hr />
+          <div>Email: {{ email }}</div>
+          <hr />
+          <div>Senha: {{ senha }}</div>
+          <hr />
+          <div>Senha: {{ confirmacao }}</div>
+          <hr />
+          <div>Profissao: {{ profissao }}</div>
+          <hr />
+          <div>Salario: {{ salario }}</div>
+          <hr />
+          <div>CPF: {{ cpf }}</div>
+          <hr />
+          <div>Idade: {{ idade }}</div>
+          <hr />
+          <div>Data: {{ data }}</div>
+          <hr />
+          <div>Endereco: {{ Endereco }}</div>
+          <hr />
+          <div>Estado: {{ cidade }}</div>
+          <hr />
+          <div>Biografia: {{ biografia }}</div>
+          <hr />
+          <div>Linguagem: {{ linguagens }}</div>
+          <button @click="deletar(apagado)">Deletar</button>
+          <button @click="edit(editar)">Editar</button>
+        </div>
       </div>
-      <div>
-      Nome: {{ nome }}
-    </div>
-      <hr>
-    <div>
-      Email: {{ email }}
-    </div>
-    <hr>
-    <div>
-      Senha: {{ senha }}
-    </div>
-    <hr>
-    <div>
-      Senha: {{ confirmacao }}
-    </div>
-    <hr>
-    <div>
-      Profissao: {{ profissao }}
-    </div>
-    <hr>
-    <div>
-      Salario: {{ salario }}
-    </div>
-    <hr>
-      <div>
-      CPF: {{ cpf }}
-    </div>
-      <hr>
-      <div>
-      Idade: {{ idade }}
-    </div>
-    <hr>
-    <div>
-      Data: {{ data }}
-    </div>
-    <hr>
-    <div>
-      Endereco: {{ Endereco }}
-    </div>
-    <hr>
-    <div>
-      Estado: {{ cidade }}
-    </div>
-    <hr>
-    <div>
-      Biografia: {{ biografia }}
-    </div>
-    <hr>
-    <div>
-      Linguagem: {{ linguagens }}
-    </div>
-    <button @click="deletar(apagado)">Deletar</button>
-    <button @click="edit(editar)">Editar</button>
-  </div>
-    </div>
     </div>
   </div>
-
 </template>
 
 <style scoped>
@@ -218,7 +327,6 @@ function handleFileUpload(e) {
   padding: 25px;
   background-color: rgb(187, 178, 178);
   margin: 0px auto;
- 
 }
 .salvas {
   border-radius: 40px;
@@ -228,7 +336,6 @@ function handleFileUpload(e) {
   position: fixed;
 }
 
-
 h1 {
   color: rgb(231, 16, 16);
   font-weight: bold 10px;
@@ -236,36 +343,32 @@ h1 {
 }
 
 .user-cards {
-    display: flow-root;
-    flex-wrap: wrap;
-    justify-content: flex-end;
-    margin-top: 20px;
-  }
+  display: flow-root;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
 
-  .card {
-    flex: 0 0 300px;
-    background-color: #918989;
-    border-radius: 15px;
-    box-shadow: 0 2px 4px rgb(255, 0, 0);
-    margin-right: 0px;
-    margin-bottom: 20px;
-  }
+.card {
+  flex: 0 0 300px;
+  background-color: #918989;
+  border-radius: 15px;
+  box-shadow: 0 2px 4px rgb(255, 0, 0);
+  margin-right: 0px;
+  margin-bottom: 20px;
+}
 
-  .card-content {
-    padding: 30px;
-  }
+.card-content {
+  padding: 30px;
+}
 
-  .card button {
-    padding: 5px 10px;
-    margin-top: 10px;
-    border-radius: 3px;
-    border: none;
-    color: #bd1313;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-
- 
-
-
+.card button {
+  padding: 5px 10px;
+  margin-top: 10px;
+  border-radius: 3px;
+  border: none;
+  color: #bd1313;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
 </style>
